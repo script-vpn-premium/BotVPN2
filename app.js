8f78f599e388d60cc74431ae913063ff24603dfd
Riswan Store <serverpremium32@gmail.com>
2025-05-24 21:39:02 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -2130,7 +2130,7 @@ async function handleDepositState(ctx, userId, data) {
     if (currentAmount.length === 0) {
       return await ctx.answerCbQuery('⚠️ Jumlah tidak boleh kosong!', { show_alert: true });
     }
-    if (parseInt(currentAmount) < 1000) {
+    if (parseInt(currentAmount) < 1) {
       return await ctx.answerCbQuery('⚠️ Jumlah minimal top-up adalah  1.000 Ya Kak...!!!', { show_alert: true });
     }
     global.depositState[userId].action = 'confirm_amount';
